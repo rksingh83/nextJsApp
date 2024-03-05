e7b7d357c6086cc20f6030b944408123b349b690
Rakesh_singhA16924 <rakeshsingh@aesl.in>
2024-03-05 12:14:34 +0530

    react icons package added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,16 @@
-import Image from 'next/image'
-import logo from '@/assets/images/logo-white.png'
-import profileDefault from '@/assets/images/profile.png'
-
+"use client";
+import Image from "next/image";
+import logo from "@/assets/images/logo-white.png";
+import profileDefault from "@/assets/images/profile.png";
+import Link from "next/link";
+import { FaGoogle } from "react-icons/fa";
+import { useState } from "react";
 const Navbar = () => {
-
+  const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
-              <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-20 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
             <button
@@ -38,37 +41,33 @@ const Navbar = () => {
 
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             {/* <!-- Logo --> */}
-            <a className="flex flex-shrink-0 items-center" href="/index.html">
-              <Image
-                className="h-10 w-auto"
-                src= {logo}
-                alt="PropertyPulse"
-              />
+            <Link className="flex flex-shrink-0 items-center" href="">
+              <Image className="h-10 w-auto" src={logo} alt="PropertyPulse" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 PropertyPulse
               </span>
-            </a>
+            </Link>
             {/* <!-- Desktop Menu Hidden below md screens --> */}
             <div className="hidden md:ml-6 md:block">
               <div className="flex space-x-2">
-                <a
-                  href="/index.html"
+                <Link
+                  href="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Home
-                </a>
-                <a
-                  href="/properties.html"
+                </Link>
+                <Link
+                  href="/properties"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Properties
-                </a>
-                <a
-                  href="/add-property.html"
+                </Link>
+                <Link
+                  href="/properties/add-property"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Add Property
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -77,7 +76,7 @@ const Navbar = () => {
           <div className="hidden md:block md:ml-6">
             <div className="flex items-center">
               <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                <i className="fa-brands fa-google text-white mr-2"></i>
+                <FaGoogle className="mr-2 text-white" />
                 <span>Login or Register</span>
               </button>
             </div>
@@ -85,7 +84,7 @@ const Navbar = () => {
 
           {/* <!-- Right Side Menu (Logged In) --> */}
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-            <a href="messages.html" className="relative group">
+            <Link href="messages" className="relative group">
               <button
                 type="button"
                 className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -111,7 +110,7 @@ const Navbar = () => {
                 2
                 {/* <!-- Replace with the actual number of notifications --> */}
               </span>
-            </a>
+            </Link>
             {/* <!-- Profile dropdown button --> */}
             <div className="relative ml-3">
               <div>
@@ -126,7 +125,7 @@ const Navbar = () => {
                   <span className="sr-only">Open user menu</span>
                   <Image
                     className="h-8 w-8 rounded-full"
-                    src= {profileDefault}
+                    src={profileDefault}
                     alt=""
                   />
                 </button>
@@ -141,33 +140,32 @@ const Navbar = () => {
                 aria-labelledby="user-menu-button"
                 tabIndex="-1"
               >
-                <a
-                  href="/profile.html"
+                <Link
+                  href="/profile"
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-0"
                 >
                   Your Profile
-                </a>
-                <a
-                  href="saved-properties.html"
+                </Link>
+                <Link
+                  href="properties/saved"
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-2"
                 >
                   Saved Properties
-                </a>
-                <a
-                  href="#"
+                </Link>
+                <button
                   className="block px-4 py-2 text-sm text-gray-700"
                   role="menuitem"
                   tabIndex="-1"
                   id="user-menu-item-2"
                 >
                   Sign Out
-                </a>
+                </button>
               </div>
             </div>
           </div>
@@ -175,28 +173,28 @@ const Navbar = () => {
       </div>
 
       {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-      <div className="hidden" id="mobile-menu">
+      <div id="mobile-menu">
         <div className="space-y-1 px-2 pb-3 pt-2">
-          <a
-            href="/index.html"
+          <Link
+            href="/"
             className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
           >
             Home
-          </a>
-          <a
-            href="/properties.html"
+          </Link>
+          <Link
+            href="/properties"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
             Properties
-          </a>
-          <a
-            href="/add-property.html"
+          </Link>
+          <Link
+            href="/property/add"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
             Add Property
-          </a>
+          </Link>
           <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-            <i className="fa-brands fa-google mr-2"></i>
+            <FaGoogle className="mr-2 text-white" />
             <span>Login or Register</span>
           </button>
         </div>
